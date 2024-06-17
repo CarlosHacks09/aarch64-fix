d90ce4972aea80fb7670b2ea9c57ef7cd20f469c
Carlos Santiago Ramírez Amezcua <134228961+CarlosHacks09@users.noreply.github.com>
2024-06-17 16:08:41 -0600

    Add noble script

---
+++ noble.sh
@@ -0,0 +1,15 @@
+echo "Are you root? If you are not root, this script will have no effect on your system. Continue?"
+read -n1 -r -p "Press any key to continue..." key
+dpkg --add-architecture i386
+dpkg --add-architecture armhf
+sed 's/^deb http/deb [arch=arm64] http/' -i '/etc/apt/sources.list'
+touch /etc/apt/sources.list.d/i386.list
+echo "deb [arch=i386] http://security.ubuntu.com/ubuntu/ noble-security main restricted universe multiverse" | tee /etc/apt/sources.list.d/i386.list
+echo "deb [arch=i386] http://archive.ubuntu.com/ubuntu/ noble main restricted universe multiverse" | tee -a /etc/apt/sources.list.d/i386.list
+echo "deb [arch=i386] http://archive.ubuntu.com/ubuntu/ noble-updates main restricted universe multiverse" | tee -a /etc/apt/sources.list.d/i386.list
+echo "deb [arch=i386] http://archive.ubuntu.com/ubuntu/ noble-backports main restricted universe multiverse" | tee -a /etc/apt/sources.list.d/i386.list
+sudo sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list
+sudo add-apt-repository universe
+sudo apt update
+sudo apt full-upgrade -y
+sudo reboot
